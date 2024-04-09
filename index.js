c7d65d5565ca6764a9bc0b040e30b8239a0eb257
gideon rotich <gideonrotich2001@gmail.com>
2024-04-09 22:53:00 +0300

    complete

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // code your solution here
+function superbowlWin (records) {
+    let win = records.find(record => record.result === "W");
+    return win ? win.year : undefined
+ }
+
+ superbowlWin()
+
+console.log(superbowlWin());
